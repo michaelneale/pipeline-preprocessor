c4d1898d718c01a1ac793032cc4ac264cc3db311
Michael Neale <michael.neale@gmail.com>
2016-07-13 15:43:05 +1000

    just the basics

--- src/main/java/preprocessor/App.java
+++ src/main/java/preprocessor/App.java
@@ -10,4 +10,46 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+
+
+    public String insertinator(final String line) {
+        String[] tokens = line.trim().split("\\s+");
+        if (lastToken(tokens).equals("{")
+                && isAtom(tokens[0])
+                && missingParen(tokens[1])) {
+
+            String stepName = tokens[0];
+            String oldSnippet = line.substring(line.indexOf(stepName), line.lastIndexOf("{"));
+            String args = line.substring(line.indexOf(stepName) + stepName.length(), line.lastIndexOf("{")).trim();
+            if (args.equals("")) {
+                return line;
+            } else {
+                String newSnippet = tokens[0] + "(" + args + ") ";
+                return line.replace(oldSnippet, newSnippet);
+            }
+
+
+        } else {
+            return line;
+        }
+
+
+    }
+
+    private boolean missingParen(String token) {
+        return !token.startsWith("(");
+    }
+
+    private String lastToken(String[] tokens) {
+        return tokens[tokens.length - 1];
+    }
+
+
+    private boolean isAtom(String token) {
+        if (token.contains("(") && Character.isLetter(token.charAt(0))) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
--- src/test/java/preprocessor/AppTest.java
+++ src/test/java/preprocessor/AppTest.java
@@ -31,8 +31,18 @@ public class AppTest
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
+    public void testApp() {
         assertTrue( true );
     }
+
+    public void testInsertinatorSimple() {
+        App a = new App();
+        assertEquals("yeah", a.insertinator("yeah"));
+        assertEquals("stage('yeah') {", a.insertinator("stage('yeah') {"));
+        assertEquals(" stage ('yeah') { ", a.insertinator(" stage ('yeah') { "));
+        assertEquals("stage('yeah') {", a.insertinator("stage 'yeah' {"));
+        assertEquals("stage(faux:'yeah', woo:2) {", a.insertinator("stage faux:'yeah', woo:2   {"));
+        assertEquals("timestamps {", a.insertinator("timestamps {"));
+    }
+
 }
