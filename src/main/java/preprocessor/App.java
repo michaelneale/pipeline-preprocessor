16922653967259cfc6042b5a7df4b93fd6911852
Michael Neale <michael.neale@gmail.com>
2016-07-13 12:12:44 +1000

    first post

---
+++ src/main/java/preprocessor/App.java
@@ -0,0 +1,13 @@
+package preprocessor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
